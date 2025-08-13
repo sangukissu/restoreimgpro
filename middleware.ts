40fba0d7a97403e4bd47f8d4fd6454d99bf23cce
sangukissu <sangukissu@gmail.com>
2025-08-13 11:39:37 +0530

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -5,15 +5,9 @@ export async function middleware(request: NextRequest) {
   return await updateSession(request)
 }
 
+// Only run middleware on /dashboard and its subroutes
 export const config = {
   matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     * - public files (images, etc.)
-     */
-    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp|css|js)$).*)',
+    '/dashboard/:path*'
   ],
 }
